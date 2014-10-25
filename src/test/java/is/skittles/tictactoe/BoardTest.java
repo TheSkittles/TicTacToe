371ecd06ebb56367e7f41b0fbe7ed9b57d7b5593
Lilja <liljaos11@ru.is>
2014-10-25 17:26:42 +0000

    Added method for marking cell and getting cell value, and test for that

--- src/test/java/is/skittles/tictactoe/BoardTest.java
+++ src/test/java/is/skittles/tictactoe/BoardTest.java
@@ -5,8 +5,21 @@ import static org.junit.Assert.*;
 
 
 public class BoardTest {
+
+	Board b = new Board();
+
     @Test public void testArraySize() {
-        Board b = new Board();
         assertEquals(b.size(), 9);
     }
+
+    @Test public void testPlayerXmarksAtCell0() {
+        b.mark(0, 1);
+        assertEquals(b.getCellValue(0), 1);
+    }
+
+   /* @Test public void testPlayerXmarksAtCell0() {
+        Board b = new Board();
+        b.mark(0, 1);
+        assertEquals(b.getCellValue(0), 1);
+    }*/
 }
