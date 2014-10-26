5e3a274ca1f11faa31732efe1774b8e2f2c462bc
Darri <darrir13@ru.is>
2014-10-26 15:29:02 +0000

    Refactor Board by changing it to two dimensional array

--- src/main/java/is/skittles/tictactoe/Board.java
+++ src/main/java/is/skittles/tictactoe/Board.java
@@ -1,31 +1,34 @@
 package is.skittles.tictactoe;
 
+
 public class Board {	
 
 	//0 eqauls empty cell, 1 equals x and 2 equals o
-	private int[] board = new int[9];
+	
+    private int n = 3;
+    private int[][] board = new int[n][n];
 
 	Board(){};
 
-	public int size(){
-		return board.length;
-	}
-
 	//cell is the cell to be marked, team equals the team playing
     public void mark(int cell, int team){
+
     	if(cell<0||cell>8){
             throw new IndexOutOfBoundsException("Cell out of bound");
         }
         if(team<1||team>2){
             throw new IndexOutOfBoundsException("Team can only be 1 or 2");
         }
-        if(board[cell] == 0) board[cell] = team;
+        if(board[(int)(Math.floor(cell/n))][cell % n] == 0) 
+            board[(int)(Math.floor(cell/n))][cell % n] = team;
+
     }
 
     public int getCellValue(int cell){
         if(cell<0||cell>8){
             throw new IndexOutOfBoundsException("Cell out of bound");
         }
-    	return board[cell];
+    	return board[(int)(Math.floor(cell/n))][cell % n];
     }
+
 }
