f8be73bc167aa43c56fc76474dc44ab32c83f1e0
Erlingur Þorsteinsson <erlingur@erlingur.is>
2014-10-27 02:32:02 +0000

    Add index.html and css styles
    Use normalize.css to standardize CSS styles between browsers and operating
    systems.

--- src/main/java/is/skittles/tictactoe/TicTacToeWeb.java
+++ src/main/java/is/skittles/tictactoe/TicTacToeWeb.java
@@ -4,6 +4,9 @@ import static spark.Spark.*;
 
 public class TicTacToeWeb {
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
         get("/hello", (req, res) -> "Hello World");
     }
 }
