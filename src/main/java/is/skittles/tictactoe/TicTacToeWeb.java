2344017791d465129818451524b3d8a6558b474d
Erlingur Þorsteinsson <erlingur@erlingur.is>
2014-10-27 15:06:29 +0000

    Can now pass in marks param and find winner
    If you pass in the ?marks parameter you can pass in a list of marks
    starting with player 1. After all the marks have been entered we attempt
    to find a winner and display it if the winner was found.

--- src/main/java/is/skittles/tictactoe/TicTacToeWeb.java
+++ src/main/java/is/skittles/tictactoe/TicTacToeWeb.java
@@ -15,8 +15,27 @@ public class TicTacToeWeb {
 
         // GameWeb.mustache file is in resources/templates directory
         get("/", (rq, rs) -> {
+          String marks = rq.queryParams("marks");
+
           Map map = new HashMap();
-          map.put("name", "Erlingur");
+          map.put("marks", marks);
+          boolean hasWinner = false;
+
+          if (marks != null && marks.length() > 0) {
+            Board b = new Board();
+            Game g = new Game(b);
+
+            for (char ch: marks.toCharArray()) {
+              g.mark(Character.getNumericValue(ch));
+            }
+
+            if (g.getWinner() > 0) {
+              map.put("winner", g.getWinner());
+              hasWinner = true;
+            }
+          }
+
+          map.put("hasWinner", hasWinner);
 
           return new ModelAndView(map, "TicTacToeWeb.mustache");
         }, new MustacheTemplateEngine());
