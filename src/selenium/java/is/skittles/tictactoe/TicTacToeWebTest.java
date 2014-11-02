d78979370a5ef7af6aa5e4f31dd763d2ab7ec963
Erlingur Þorsteinsson <erlingur@erlingur.is>
2014-11-02 13:41:23 +0000

    This selenium test should fail and not deploy to prod

--- src/selenium/java/is/skittles/tictactoe/TicTacToeWebTest.java
+++ src/selenium/java/is/skittles/tictactoe/TicTacToeWebTest.java
@@ -35,6 +35,12 @@ public class TicTacToeWebTest {
     assertEquals(0, driver.findElements(By.cssSelector("p.draw")).size());
   }
 
+  @Test
+  public void testNoInsultForUnoriginalMarks() throws Exception {
+    driver.get(baseUrl + "/?marks=0123");
+    assertEquals(0, driver.findElements(By.cssSelector("p.error")).size());
+  }
+
   @After
   public void tearDown() throws Exception {
     driver.quit();
